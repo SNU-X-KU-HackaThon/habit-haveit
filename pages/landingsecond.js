142a4452072929f014988baa667c1ac64d6a601d
Phia(가영) <76558220+rkdud007@users.noreply.github.com>
2022-01-28 14:06:17 +0900

    landing

--- pages/landingsecond.js
+++ pages/landingsecond.js
@@ -12,7 +12,7 @@ export default function Landingsecond() {
         if (index === 2) {
           console.log("hi");
         } else {
-          if (index === 5) {
+          if (index === 7) {
             Router.push("/auth");
           } else {
             setIndex(index + 1);
@@ -39,7 +39,9 @@ export default function Landingsecond() {
             </div>
           ) : index === 4 ? (
             <div className={classes.text}>
-              한달에 하나씩, 나만의 어드벤트 캘린더를 만들고
+              한달에 하나씩,나만의
+              <br />
+              어드벤트 캘린더를 만들어,
               <br />
               이달의 목표를 정하고
               <br />
@@ -49,10 +51,25 @@ export default function Landingsecond() {
             <div className={classes.text}>
               매일 목표를 달성하면
               <br />
-              체크박스를 눌러 문을 열어봐요!
+              체크박스를 눌러
               <br />
-              문속에 친구들의 편지가 들어있네요! <br />
-              지금부터 한달 목표를 이뤄볼까요?
+              문을 열어봐요!
+            </div>
+          ) : index === 6 ? (
+            <div className={classes.text}>
+              문속에 친구들의
+              <br />
+              편지가 들어있고, <br />
+              <br />
+              선물 박스에는 친구들의
+              <br /> 선물들이 들어있어요
+            </div>
+          ) : index === 7 ? (
+            <div className={classes.text}>
+              자그럼, 지금부터 <br />
+              <br />
+              이달의 멋진 목표를 세우고
+              <br /> 친구들에게 공유해봐요!
             </div>
           ) : null}
         </div>
@@ -63,6 +80,7 @@ export default function Landingsecond() {
             <Button onClick={() => setIndex(index + 1)}>네! 0_0</Button>
 
             <Button onClick={() => setIndex(index + 1)}>아직이요!</Button>
+            <Button onClick={() => Router.push("/auth")}>스킵할래요!</Button>
           </div>
         ) : (
           <div className={classes.buttons} />
