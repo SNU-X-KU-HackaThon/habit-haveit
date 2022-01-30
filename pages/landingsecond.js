52d9f0ec872b406fecf3441a9c1c593086862e35
Phia(가영) <76558220+rkdud007@users.noreply.github.com>
2022-01-30 23:53:10 +0900

    edit

--- pages/landingsecond.js
+++ pages/landingsecond.js
@@ -5,6 +5,7 @@ import Button from "../src/components/Button";
 
 export default function Landingsecond() {
   const [index, setIndex] = useState(2);
+  const [check, setCheck] = useState(false);
   return (
     <div
       className={classes.highwrap}
@@ -56,9 +57,83 @@ export default function Landingsecond() {
             <Button onClick={() => setIndex(index + 1)}>아직이요!</Button>
             <Button onClick={() => Router.push("/auth")}>스킵할래요!</Button>
           </div>
-        ) : index === 3 || index === 4 || index === 5 ? (
+        ) : index === 3 || index === 4 ? (
           <div className={classes.buttons}>
             <img src="../adventcalendar.gif" />
+          </div>
+        ) : index === 5 ? (
+          <div className={classes.mainFooter}>
+            <div>
+              {check ? (
+                <img style={{ opacity: 0.2 }} src="../checkboxs.png" />
+              ) : (
+                <img style={{ opacity: 0.2 }} src="../empty.png" />
+              )}
+            </div>
+
+            <div>
+              {" "}
+              <img src="../group.png" />
+            </div>
+
+            <div>
+              <img style={{ opacity: 0.2 }} src="../vector.png" />
+            </div>
+          </div>
+        ) : index === 7 ? (
+          <>
+            {check ? (
+              <img
+                style={{ zIndex: 100 }}
+                src={`../openDoors/1.png`}
+                width="80px"
+                height="160px"
+              />
+            ) : (
+              <img
+                style={{ zIndex: 100 }}
+                src={`../doors/1.png`}
+                width="80px"
+                height="160px"
+              />
+            )}
+            <div className={classes.mainFooter}>
+              <div>
+                {check ? (
+                  <img src="../checkboxs.png" />
+                ) : (
+                  <img src="../empty.png" />
+                )}
+              </div>
+
+              <div>
+                {" "}
+                <img style={{ opacity: 0.2 }} src="../group.png" />
+              </div>
+
+              <div>
+                <img style={{ opacity: 0.2 }} src="../vector.png" />
+              </div>
+            </div>
+          </>
+        ) : index === 8 ? (
+          <div className={classes.mainFooter}>
+            <div>
+              {check ? (
+                <img style={{ opacity: 0.2 }} src="../checkboxs.png" />
+              ) : (
+                <img style={{ opacity: 0.2 }} src="../empty.png" />
+              )}
+            </div>
+
+            <div>
+              {" "}
+              <img style={{ opacity: 0.2 }} src="../group.png" />
+            </div>
+
+            <div>
+              <img src="../vector.png" />
+            </div>
           </div>
         ) : (
           <div className={classes.buttons} />
